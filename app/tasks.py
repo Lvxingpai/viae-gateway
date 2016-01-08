c194cfa777f4c05eb04b737ff0e0d9ca6d361d46
golmic <ljq0225@gmail.com>
2016-01-08 18:38:59 +0800

    MOD 修正提出的几个问题

--- app/tasks.py
+++ app/tasks.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 client = MongoClient(MONGO_URL)
 
 
-@celery.task(name='mongo_to_celery', bind=True, acks_late=True)
+@celery.task(name='viae.gateway.stashTask', bind=True, acks_late=True)
 def mongo_to_celery():
     coll = client.viae.ViaeTask
     tasks = coll.find({'eta': {'$lt': datetime.now() + timedelta(seconds=TASK_ETA_THRESHOLD)}})
@@ -15,8 +15,9 @@ def mongo_to_celery():
         kwargs = task.get('kwargs', {})
         eta = task.get('eta')
         expires = task.get('expires')
+        id = task.get('_id')
         task = task['task']
-        result = celery.send_task(task, serializer='json', args=args, kwargs=kwargs, eta=eta,
-                                  expires=expires)
-        delete = coll.remove({'task':task})
-    mongo_to_celery.apply_async(countdown=60)
+        celery.send_task(task, serializer='json', args=args, kwargs=kwargs, eta=eta,
+                         expires=expires)
+        coll.remove({'_id': id})
+    mongo_to_celery.apply_async(countdown=TASK_POLLING_INTERVAL)
